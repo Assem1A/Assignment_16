01f12f26ee2da4ab119d6f97e569ffe4f8ae373c
Assem64 <asemmido0550@gmail.com>
2026-04-29 21:54:08 +0300

    Delete dist directory

--- dist/common/utils/otp.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createNumberOtp = void 0;
-const createNumberOtp = () => {
-    return Math.floor(Math.random() * (999999 - 100000 + 1) + 100000);
-};
-exports.createNumberOtp = createNumberOtp;
