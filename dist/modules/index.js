ad59542f004987dd9d8def78b0050a304f82c28f
Assem64 <asemmido0550@gmail.com>
2026-05-04 04:15:49 +0300

    Delete dist directory

--- dist/modules/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authRouter = void 0;
-var index_1 = require("./auth/index");
-Object.defineProperty(exports, "authRouter", { enumerable: true, get: function () { return index_1.authRouter; } });
